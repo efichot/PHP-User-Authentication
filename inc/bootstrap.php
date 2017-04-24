b3e5f09eb1a30a21cb549c5ef4508479df8ef5bc
Etienne Fichot <fichotetienne@gmail.com>
2017-04-24 22:22:52 +0200

    JWT added

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -3,4 +3,7 @@
 	require_once __DIR__ . '/functions.php';
 	require_once __DIR__ . '/connection.php';
 
+	//Where find the .env file
+	$doenv = new Dotenv\Dotenv(__DIR__);
+	$doenv->load();
 ?>
--- procedures/doLogin.php
+++ procedures/doLogin.php
@@ -9,5 +9,16 @@
 	if (!password_verify(request()->get('password'), $user['password'])) {
 		header('Location: ../login.php');
 	}
-	
+
+	$expTime = time()+ 3600;
+
+	//create the JWT with encode static function. three paramaters 1/ an array the claims 2/ secret_key 3/algo
+	$jwt = \Firebase\JWT\JWT::encode([
+		'iss' => request()->getBaseUrl(), // issuer
+		'sub' => "{$user['id']}", //subject
+		'exp' => $expTime, //expiration time
+		'iat' =>time(), //issued time
+		'nbf' => time(), //Not before
+		'is_admin' => $user['role_id'] == 1 //return ture or false
+	], getenv(SECRET_KEY), 'HS256');
  ?>
